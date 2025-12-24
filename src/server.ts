27c40566f14e4a285428c4b40023020416386899
ddw-wms <ddw.blr@gmail.com>
2025-12-24 13:16:15 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://backend-ho59.onrender.com'
+    'https://wms-ddw.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
@@ -135,3 +135,4 @@ app.use(errorHandler);
 
 
 export default app;
+
